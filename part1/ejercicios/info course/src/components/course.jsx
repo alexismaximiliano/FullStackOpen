fa69fd21a79aefc0dacfd0b00184035d1abcb199
alexismaximiliano <alexismaxi97@gmail.com>
2024-04-15 13:03:01 -0300

    2.1 paso 6

---
+++ part1/ejercicios/info course/src/components/course.jsx
@@ -0,0 +1,21 @@
+import Header from "./header";
+import Content from "./content";
+import Total from "./total";
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total
+        total={
+          course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises
+        }
+      />
+    </div>
+  );
+};
+
+export default Course;
