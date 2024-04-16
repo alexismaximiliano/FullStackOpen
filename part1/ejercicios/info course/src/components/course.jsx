4de2178dd1c465a4571e419099f8efc6ef90fbbe
alexismaximiliano <alexismaxi97@gmail.com>
2024-04-16 14:11:24 -0300

    2.5 paso 10

--- part1/ejercicios/info course/src/components/course.jsx
+++ part1/ejercicios/info course/src/components/course.jsx
@@ -2,18 +2,14 @@ import Header from "./header";
 import Content from "./content";
 import Total from "./total";
 
-const Course = ({course}) => {
+const Course = ({ course }) => {
+   const total = course.parts.reduce((suma, part) => suma + part.exercises, 0);
+
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
-      <Total
-        total={
-          course.parts[0].exercises +
-          course.parts[1].exercises +
-          course.parts[2].exercises
-        }
-      />
+      <Total total={total} />
     </div>
   );
 };
--- part1/ejercicios/info course/src/components/total.jsx
+++ part1/ejercicios/info course/src/components/total.jsx
@@ -1,4 +1,4 @@
-const Total = ({total}) => {
-    return <p>Number of exercises {total} </p>;
-  };
-  export default Total;
+const Total = ({ total }) => {
+  return <b>Total of {total} exercises</b>;
+};
+export default Total;
